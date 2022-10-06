0017fbeb669c0d60de8b221d1eb78facae9f1d5b
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2022-10-06 04:12:50 +0000

    begin simulations to benchmark batch correction effects on tensor decomposition

--- env_setup/setup_env.r
+++ env_setup/setup_env.r
@@ -9,5 +9,5 @@ nmf.url<-'https://cran.r-project.org/src/contrib/Archive/NMF/NMF_0.23.0.tar.gz'
 repotools::install.pkgs('NMF', force = F, quiet = T) # how to specify version?
 
 remotes::install_github('sqjin/CellChat@c982c7132386454b3611371a29c4593b3c4a01cc', upgrade=F)
-
 remotes::install_github('saezlab/liana@f7ec81b18eb63bc7a7f941e8e2195139f6018b9c', upgrade=F)
+remotes::install_github('theislab/kBET@f35171dfb04c7951b8a09ac778faf7424c4b6bc0', upgrade = F)
