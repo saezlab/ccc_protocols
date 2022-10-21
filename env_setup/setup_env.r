f86899e03eefb708f762043011f60fb502bcde3e
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2022-10-21 22:35:26 +0000

    add packages for interoperability, remove r-essentials which installs jupyter

--- env_setup/setup_env.r
+++ env_setup/setup_env.r
@@ -11,3 +11,5 @@ repotools::install.pkgs('NMF', force = F, quiet = T) # how to specify version?
 remotes::install_github('sqjin/CellChat@c982c7132386454b3611371a29c4593b3c4a01cc', upgrade=F)
 remotes::install_github('saezlab/liana@f7ec81b18eb63bc7a7f941e8e2195139f6018b9c', upgrade=F)
 remotes::install_github('theislab/kBET@f35171dfb04c7951b8a09ac778faf7424c4b6bc0', upgrade = F)
+
+remotes::install_github("mojaveazure/seurat-disk@9b89970eac2a3bd770e744f63c7763419486b14c", upgrade=F)
