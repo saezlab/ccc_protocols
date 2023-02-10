2eff632f8ff561cea11a5862ed5d5a5c3b24a4d8
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2023-02-10 15:35:50 -0800

    update environment to account for benchmarking being a submodule

--- env_setup/setup_env.r
+++ env_setup/setup_env.r
@@ -9,9 +9,6 @@ library(repotools)
 repotools::install.pkgs('NMF', force = F, quiet = T) # how to specify version?
 
 remotes::install_github('sqjin/CellChat@c982c7132386454b3611371a29c4593b3c4a01cc', upgrade=F)
-remotes::install_github('saezlab/liana@f7ec81b18eb63bc7a7f941e8e2195139f6018b9c', upgrade=F)
-remotes::install_github('theislab/kBET@f35171dfb04c7951b8a09ac778faf7424c4b6bc0', upgrade = F)
+remotes::install_github('saezlab/liana@10d81773e0874de676eb106ce56e3cf9d4fe01d3', upgrade=F)# most recent
 
 remotes::install_github("mojaveazure/seurat-disk@9b89970eac2a3bd770e744f63c7763419486b14c", upgrade=F)
-
-remotes::install_github("keepsimpler/StabEco@fe71129dda26db1a0578557808960460e09201f6", upgrade = F)
